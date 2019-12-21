53efbe64fb65cefa62ec6077d92721ffef28832a
MetalcoreLoli <gogs@fake.local>
2019-12-21 21:21:26 +0300

    add screen

--- Core/Controls/Screen.cs
+++ Core/Controls/Screen.cs
@@ -8,29 +8,89 @@ namespace SeeBattle.Core.Controls
 {
     internal class Screen : Control
     {
+        #region Private Members
+
+        private new Cell[] body;
+
+        #endregion
+
         #region Public Properties
-        public List<Control> Controls { get; set; }
+        internal List<Control> Controls { get; set; }
+        #endregion
+
+
+        #region Events
+
         #endregion
 
         #region Constructors
-        public Screen()
+        public Screen() : this(10, 10)
         {
-            Controls = new List<Control>();
+        
         }
 
-        public Screen(Int32 width, Int32 height, params Control[] controls)
+
+        public Screen(Int32 width, Int32 height) 
+        {
+            Widht   = width;
+            Height  = height;
+            Initialization();
+            Controls = new List<Control>();
+
+        }
+
+        public Screen(Int32 width, Int32 height, params Control[] controls) : this(width, height)
         {
             Controls = new List<Control>(controls);
         }
         #endregion
 
+        #region Private Methods
+
+        private void Initialization()
+        {
+            body = InitBody(Widht, Height);
+            DrawWallsAndAngels();
+        }
+
+        private void DrawWallsAndAngels()
+        {
+            body = Control.DrawUpDownWalls(body, Widht, Height);
+            body = Control.DrawLeftRightWalls(body, Widht, Height);
+            body = Control.DrawAngels(body, Widht, Height);
+        }
+
+        private void OnUpdate()
+        {
+
+        }
+
+        #endregion
+
         #region Public Methods
+
+        public void Clean()
+        {
+            for (int i = 0; i < Widht * Height; i++)
+                body[i].Symbol = ' ';
+            DrawWallsAndAngels();
+        }
+
+        public void Update()
+        {
+            Draw();
+            OnUpdate();
+        }
+
         public override void Draw()
         {
+            Console.Clear();
+            Clean();
+            foreach (Cell cell in body)
+                Render.WithOffset(cell, 0, 0);
+
             foreach (Control control in Controls)
-            {
                 control.Draw();   
-            }
         }
         #endregion
     }
--- Menu/Commands/HelpCommand.cs
+++ Menu/Commands/HelpCommand.cs
@@ -6,9 +6,29 @@ namespace SeeBattle.Menu.Commands
 {
     internal class HelpCommand : ICommand
     {
+        #region Private Members
+        private Int32 _screenHeight = 25;
+        private Int32 _screenWidht  = 15;
+        Core.Controls.Screen _helpScreen;
+     
+        #endregion
+
+        #region Constructors
+        public HelpCommand()
+        {
+            _helpScreen = new Core.Controls.Screen(_screenHeight, _screenWidht);
+        }
+        #endregion
+
+        #region Public Methods
         public void Execute()
         {
-            throw new Exception("TODO");
+            while (true)
+            {
+                _helpScreen.Draw();
+            }
+            // throw new Exception("TODO");
         }
+        #endregion
     }
 }
